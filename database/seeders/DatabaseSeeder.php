d33bf522bf12e955cddb411bb9caa21507ccec21
Hatta-Fahri <hattafhrii25@gmail.com>
2026-07-08 14:04:21 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,10 +21,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            SparepartSeeder::class,
             SparepartAcSeeder::class,
             TransactionSeeder::class,
             HistoricalMayTransactionSeeder::class,
+            jasaServisSeeder::class,
         ]);
     }
 }
